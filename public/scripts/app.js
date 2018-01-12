0f5318a03ac319f25352ed28a9fbf746516cdb55
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2018-01-12 15:35:54 +0530

    more es6 arrow function example added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,47 @@
 'use strict';
 
-//console.log('arrow function');
+// Argument object - no longer bound with arrow function
+var add = function add(a, b) {
+    //console.log(arguments);
+    return a + b;
+};
+console.log(add(5, 10, 15, 20, 25));
 
-// Simple function
-function square(x) {
-    return x * x;
-}
-//console.log(square(10));
+// this keyword - no longer bound
 
-// Arrow function
-// const squareArrow = (x) => {
-//     return x * x;
-// }
+var user = {
+    name: 'Hasmukh',
+    cities: ['Ahmedabad', 'Rajkot', 'Pune'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
 
-var squareArrow = function squareArrow(x) {
-    return x * x;
+        // .forEach use for facing data
+        // return this.cities.forEach((city) => {
+        //     console.log(city);
+        //     return city;
+        // });
+
+        //return this.cities.map((city) => city );
+
+        // .map use for mapping data
+        return this.cities.map(function (city) {
+            return _this.name + ' has lived in ' + city;
+        });
+    }
 };
 
-//console.log(squareArrow(50));
+console.log(user.printPlacesLived());
 
-var getFirstName = function getFirstName(fullName) {
-    return fullName.split(' ')[0];
+var multiplier = {
+    numbers: [10, 20, 30],
+    multiplyBy: 5,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
 };
 
-console.log(getFirstName('Vipul Baldaniya'));
+console.log(multiplier.multiply());
