a6ce103d4f4da99824daf85cd8ca37aaf45558fb
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2018-01-13 11:00:30 +0530

    visibility toggle added

---
+++ src/playground/build-it-visible.jsx
@@ -0,0 +1,19 @@
+let visibility = false;
+
+const visibleToggleEvent = () => {
+    visibility = !visibility;
+    render();
+}
+
+const render = () => {
+    const toggle = (
+        <div>
+            <button type="button" onClick={visibleToggleEvent} >{visibility ? 'Hide' : 'Show'} Details</button>
+            {visibility && (<div><p>This is a toggle content</p></div>)}
+        </div>
+    );
+
+    ReactDOM.render(toggle, document.getElementById('app'));
+}
+
+render();
