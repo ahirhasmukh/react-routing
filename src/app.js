ca7333ce5b7186988437609898286170c7ef28ac
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2018-04-04 17:32:39 +0530

    webpack code added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+// import './utility';
+//var validator = require('validator');
+import validator from 'validator';
+import React from  'react';
+import ReactDOM from 'react-dom';
+
+import './person.jsx';
+import canDrink123, {isAdult} from './person.jsx';
+import {square , add} from './utility';
+
 console.log('app running');
+ console.log(square(3));
+// console.log(add(5,5));
+// console.log(isAdult(19));
+// console.log(canDrink123(2));
+
+console.log(validator.isEmail('foo@gmail.com'));
---
+++ src/person.jsx
@@ -0,0 +1,5 @@
+const isAdult = (age) => age >= 18 ? true : false;
+
+export default (age) => age >= 21 ? true : false;
+
+export {isAdult};
