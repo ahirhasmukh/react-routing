5ae0872beae8c0123e1be07326349fe238916cb7
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2018-05-08 15:22:39 +0530

    Separate component added

---
+++ src/components/header-component.js
@@ -0,0 +1,13 @@
+import React from  'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <NavLink to="/" activeClassName="is-active" exact={true}> Home </NavLink>
+        <NavLink to="/create" activeClassName="is-active"> Create </NavLink>
+        <NavLink to="/edit" activeClassName="is-active"> Edit </NavLink>
+        <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
+    </header>
+);
+
+export default Header;
--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -6,27 +6,28 @@ import CreateComponent from '../components/create-component';
 import HelpComponent from '../components/help-component';
 import EditComponent from '../components/edit-component';
 import NotFoundComponent from '../components/not-found';
+import Header from '../components/header-component';
 
-const Header = () => (
-    <header>
-        <NavLink to="/" activeClassName="is-active" exact={true}> Home </NavLink>
-        <NavLink to="/create" activeClassName="is-active"> Create </NavLink>
-        <NavLink to="/edit" activeClassName="is-active"> Edit </NavLink>
-        <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
-    </header>
-)
+// const Header = () => (
+//     <header>
+//         <NavLink to="/" activeClassName="is-active" exact={true}> Home </NavLink>
+//         <NavLink to="/create" activeClassName="is-active"> Create </NavLink>
+//         <NavLink to="/edit" activeClassName="is-active"> Edit </NavLink>
+//         <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
+//     </header>
+// )
 
 const AppRouter = () => (
     <BrowserRouter>
         <div>
             <Header />
             <Switch>
-            <Route path="/" component={homeComponent} exact={true}/>
-            <Route path="/create" component={CreateComponent}/>
-            <Route path="/edit" component={EditComponent}/>
-            <Route path="/help" component={HelpComponent}/>
-            <Route component={NotFoundComponent}/>
-        </Switch>
+                <Route path="/" component={homeComponent} exact={true}/>
+                <Route path="/create" component={CreateComponent}/>
+                <Route path="/edit" component={EditComponent}/>
+                <Route path="/help" component={HelpComponent}/>
+                <Route component={NotFoundComponent}/>
+            </Switch>
         </div>
     </BrowserRouter>
 );
