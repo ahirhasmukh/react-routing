323ab0ad97bccb99f65ffd88d293cdaeb968ce28
Hasmukh <hasmukh.baldaniya@smartsensesolutions.com>
2018-05-12 18:14:05 +0530

    object and array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring
+//
+
 const book = {
     title : 'Ego is the Enemy',
     author: 'Ryan Holiday',
@@ -14,3 +18,31 @@ const {name:publisherName="Hasmukh"} = book.publisher;
 if(publisherName){
     console.log(`Book Publisher name is ${publisherName}` );
 }
+
+//
+// Array Destructuring
+//
+
+const address = ['C 304 Divyajivan Aura', 'Gujrat', 'Gandhinagar', '382421'];
+
+//console.log(`My address is ${address[1]}`);
+
+// const [street, state, city, zip] = address;
+
+// console.log(`My address is ${street} ${state} ${city} ${zip}`);
+
+// const [,, city, zip] = address;
+
+// console.log(`My address is  ${city} ${zip}`);
+
+
+const [, , city, ,] = address;
+
+console.log(`My address is  ${city}`);
+
+
+let cities = [];
+
+const [, , street1 = "Ahmedabad"] = cities;
+
+console.log(`My address is  ${street1}`);
